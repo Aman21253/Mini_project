9a875ddb4543c49086d5c06af65c7f8f1355f66a
Aman022411 <aman.bansal@celetel.com>
2025-11-06 18:54:46 +0530

    feat: login and create account

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const userModel = require("./models/users");
+const userModel = require("./model/user");
+const postModel = require("./model/post");
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -10,6 +13,42 @@ app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.render("index")
+})
+
+app.get('/login', (req, res) => {
+    res.render("login")
+})
+
+app.post('/register', async (req, res) => {
+    let { email, password, username, name, age } = req.body;
+    let user = await userModel.findOne({ email })
+    if (user) return res.status(500).send("User already registered");
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                username,
+                email,
+                age,
+                name,
+                password: hash,
+            });
+            let token = jwt.sign({ email: email, userid: user._id }, "xyz");
+            res.cookie("token", token);
+            res.send("You successfully registered to our platform");
+        })
+    })
+})
+
+app.post('/login', async (req, res) => {
+    let { email, password } = req.body;
+    let user = await userModel.findOne({ email })
+    if (!user) return res.status(500).send("Something went wrong");
+
+    bcrypt.compare(password, user.password, function (err, result) {
+        if (result) res.status(200).send("You can login");
+        else res.redirect('/login');
+    })
 })
 
 app.listen(3000);
